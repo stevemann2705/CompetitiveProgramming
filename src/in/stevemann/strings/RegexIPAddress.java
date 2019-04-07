b817e12567cdaad154bcdcd47c9cc45e1a1b3fc5
Steve Mann <stevemann2705@gmail.com>
2019-04-07 23:07:22 +0530

    Adding RegexIPAddress

---
+++ src/in/stevemann/strings/RegexIPAddress.java
@@ -0,0 +1,21 @@
+package in.stevemann.strings;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+import java.util.Scanner;
+
+public class RegexIPAddress{
+
+    public static void main(String[] args){
+        Scanner in = new Scanner(System.in);
+        while(in.hasNext()){
+            String IP = in.next();
+            System.out.println(IP.matches(new MyRegex().pattern));
+        }
+
+    }
+}
+
+class MyRegex{
+    public String pattern = "(25[0-5]|2[0-4][0-9]|1[0-9][0-9]|[1-9]?[0-9])\\.(25[0-5]|2[0-4][0-9]|1[0-9][0-9]|[1-9]?[0-9])\\.(25[0-5]|2[0-4][0-9]|1[0-9][0-9]|[1-9]?[0-9])\\.(25[0-5]|2[0-4][0-9]|1[0-9][0-9]|[1-9]?[0-9])";
+}
